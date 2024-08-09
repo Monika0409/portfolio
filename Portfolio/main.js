25da3e73c84afb6abd6552e93c0d2ce1856b6ccd
Monikamali0409 <monikamali8263@gmail.com>
2024-08-09 23:45:59 +0530

    commit

--- Portfolio/main.js
+++
@@ -1,7 +0,0 @@
-var typed =new typed(".text",{
-    strings :["Full Stack Developer ","Web Developer","Backend Developer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-});
